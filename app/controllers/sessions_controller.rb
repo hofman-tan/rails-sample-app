4745d3853ee10db453b59676a3beebf3b71f8894
htan0005 <htan0005@student.monash.edu>
2020-09-25 16:00:37 +0800

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])    ## user && user.auth...
+      reset_session     # invalidates the session id on the server to avoid “Session Fixation” attack
+      log_in user
+      redirect_to user
+    else
+      # flash.now disappear as soon as there is an additional request
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,8 @@ threads min_threads_count, max_threads_count
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+# I've commented out this line for rails dev server to work in Windows platform
+#workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 plugin :tmp_restart
