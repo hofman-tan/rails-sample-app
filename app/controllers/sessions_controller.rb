00adac5c48acbd3af3c7543da3bdeedd0d24d2cb
htan0005 <htan0005@student.monash.edu>
2020-09-26 12:38:10 +0800

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])    ## user && user.auth...
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])    ## user && user.auth...
       reset_session     # invalidates the session id on the server to avoid “Session Fixation” attack
-      log_in user
-      redirect_to user
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # flash.now disappear as soon as there is an additional request
       flash.now[:danger] = 'Invalid email/password combination'
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
