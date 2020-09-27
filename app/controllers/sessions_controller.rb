34f37a9d27fb3fa2bc1c7a24706d952c18907f69
htan0005 <htan0005@student.monash.edu>
2020-09-27 13:01:16 +0800

    Finish user edit, update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])    ## user && user.auth...
+      forwarding_url = session[:forwarding_url]   # pull fowarding url out before reset session (or else the url is removed)
       reset_session     # invalidates the session id on the server to avoid “Session Fixation” attack
-      log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      log_in @user
+      redirect_to forwarding_url || @user
     else
       # flash.now disappear as soon as there is an additional request
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,15 +13,23 @@ module SessionsHelper
 
   # Returns the user corresponding to the remember token cookie.
   def current_user
+    # checks if the user has explicitly logged in via the login page
     if (user_id = session[:user_id])      # check if session of user id exists
       @current_user ||= User.find_by(id: user_id)
+    # checks if the user has chosen to "remember me" even though the user might not go through the login part
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
+        # create a session for user if the user has valid cookie data
         log_in user
         @current_user = user
       end
     end
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -41,5 +49,10 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
