1c1b52e8fa96b5e76ad42a67a3c11ceae3e82822
htan0005 <htan0005@student.monash.edu>
2020-10-07 14:11:58 +0800

    Fix bug in sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_production"
 
   config.action_mailer.perform_caching = false
-  
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'secure-sands-70301.herokuapp.com'
+  host = 'https://secure-sands-70301.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
