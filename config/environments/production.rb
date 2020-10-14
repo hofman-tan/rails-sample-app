aef2c38710f2a2643a8caaeaf7ac0820cb7e5ecc
htan0005 <htan0005@student.monash.edu>
2020-10-14 11:15:42 +0800

    use Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,30 +67,32 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   
-  #config.action_mailer.delivery_method = :smtp
-  #host = 'https://secure-sands-70301.herokuapp.com'
-  #config.action_mailer.default_url_options = { host: host }
-  #ActionMailer::Base.smtp_settings = {
-  #  :address        => 'smtp.sendgrid.net',
-  #  :port           => '587',
-  #  :authentication => :plain,
-  #  :user_name      => ENV['SENDGRID_USERNAME'],
-  #  :password       => ENV['SENDGRID_PASSWORD'],
-  #  :domain         => 'heroku.com',
-  #  :enable_starttls_auto => true
-  #}
+  config.action_mailer.delivery_method = :smtp
+  host = 'secure-sands-70301.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_API_KEY'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
-host = 'secure-sands-70301.herokuapp.com'
-config.action_mailer.default_url_options = { host: host }
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'secure-sands-70301.heroku.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
+
+# Mailgun
+#host = 'secure-sands-70301.herokuapp.com'
+#config.action_mailer.default_url_options = { host: host }
+#ActionMailer::Base.smtp_settings = {
+#  :port           => ENV['MAILGUN_SMTP_PORT'],
+#  :address        => ENV['MAILGUN_SMTP_SERVER'],
+#  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+#  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+#  :domain         => 'secure-sands-70301.heroku.com',
+#  :authentication => :plain,
+#}
+#ActionMailer::Base.delivery_method = :smtp
 
 
 
